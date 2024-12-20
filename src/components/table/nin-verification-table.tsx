bf6e4b4328861317b6be2fc6d589bc0ddb8d497b
joviksdev <to.emmanuel93@gmail.com>
2024-12-20 12:24:55 +0100

    Added btn to view nin verification request and response

--- src/components/table/nin-verification-table.tsx
+++ src/components/table/nin-verification-table.tsx
@@ -1,6 +1,7 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, useState } from 'react';
 import Table from '@mui/material/Table';
 import { useMutation, useQueryClient } from 'react-query';
+import JsonFormatter from 'react-json-formatter';
 import { useTheme, TableBody, TableHead, Box } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import {
@@ -23,6 +24,7 @@ import Loader from '../loader/table-loader';
 import { useAlert, useHandleError } from 'hooks';
 import { updateVerification } from 'api';
 import CustomTableCell from './components/custom-table-cell';
+import ModalWrapper from 'components/modal/Wrapper';
 
 type Props = {
 	verifications: IVerification[] | null;
@@ -43,6 +45,18 @@ const NinVerificationTable = ({
 
 	const queryClient = useQueryClient();
 	const setAlert = useAlert();
+
+	const jsonStyle = {
+		propertyStyle: { color: 'red' },
+		stringStyle: { color: 'green' },
+		numberStyle: { color: 'darkorange' },
+	};
+
+	const [jsonData, setJsonData] = useState<string>('');
+
+	const handleViewLog = (log: any) => {
+		setJsonData(JSON.stringify(log));
+	};
 
 	// const { isLoading: isVerifyingUser } = useQuery(
 	// 	'',
@@ -97,6 +111,24 @@ const NinVerificationTable = ({
 
 	return (
 		<>
+			{jsonData && (
+				<ModalWrapper
+					title={'Request and Response Data'}
+					hasCloseButton={true}
+					closeModal={() => setJsonData('')}
+				>
+					<Box
+						sx={{
+							overflow: 'auto',
+							maxWidth: '540px',
+							width: '100%',
+							alignSelf: 'flex-start',
+						}}
+					>
+						<JsonFormatter json={jsonData} tabWith={4} jsonStyle={jsonStyle} />
+					</Box>
+				</ModalWrapper>
+			)}
 			<Box style={styles.container} sx={{ overflow: 'auto' }}>
 				<Box
 					style={styles.tableHeader as CSSProperties}
@@ -168,9 +200,21 @@ const NinVerificationTable = ({
 													>
 														{row.status}
 													</TableCell>
-													<TableCell sx={{ maxWidth: '180px' }}>
+													<TableCell sx={{ maxWidth: '280px' }}>
 														{row.status === VERIFICATION_STATUS.PENDING && (
 															<Box style={styles.verifyPushWrapper}>
+																<Button
+																	onClick={() =>
+																		handleViewLog({
+																			request: row.request,
+																			response: row.response,
+																		})
+																	}
+																	size={'small'}
+																	style={styles.button as CSSProperties}
+																>
+																	View request & response
+																</Button>
 																<Button
 																	onClick={() =>
 																		handleVerifyUser(

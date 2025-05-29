b7aa298dec4d699286ffd4e9697cd5760fd1e05b
Emmanuel Tochukwu <to.emmanuel93@gmail.com>
2025-05-29 13:17:56 +0100

    Auto airtime convert details

--- src/components/transaction-details/index.tsx
+++ src/components/transaction-details/index.tsx
@@ -128,17 +128,13 @@ const TransactionDetails: React.FC<Props> = ({
 					<TransactionItem
 						label={'Total Amount'}
 						value={formatNumberToCurrency(
-							typeof transaction?.totalAmount === 'object'
-								? transaction.totalAmount.$numberDecimal
-								: ''
+							checkAmount(transaction?.totalAmount as string)
 						)}
 					/>
 					<TransactionItem
 						label={'Total Return Amount'}
 						value={formatNumberToCurrency(
-							typeof transaction?.totalReturnAmount === 'object'
-								? transaction.totalReturnAmount.$numberDecimal
-								: ''
+							checkAmount(transaction?.totalReturnAmount)
 						)}
 					/>
 				</Box>

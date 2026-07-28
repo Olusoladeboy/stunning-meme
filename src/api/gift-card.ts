cdcddb4be51aca1120511b36ad993c132ff80e7f
Emmanuel Tochukwu <to.emmanuel93@gmail.com>
2026-07-28 13:05:46 +0100

    (feat):giftcard transaction status update

--- src/api/gift-card.ts
+++ src/api/gift-card.ts
@@ -3,24 +3,36 @@ import { ENDPOINTS, DataResponse, Transaction } from '../utilities';
 import { GenericAbortSignal } from 'axios';
 
 export const giftCardTransactions = async (
-	params: {
-		[key: string]: any;
-	},
-	signal?: GenericAbortSignal,
+  params: {
+    [key: string]: any;
+  },
+  signal?: GenericAbortSignal,
 ): Promise<DataResponse<Transaction[]>> =>
-	apiRequest({
-		url: ENDPOINTS.GiftCard,
-		method: 'GET',
-		params,
-		signal,
-	});
+  apiRequest({
+    url: ENDPOINTS.GiftCard,
+    method: 'GET',
+    params,
+    signal,
+  });
 
 export const updateGiftCardTransactions = async (payload: {
-	data: { [key: string]: any };
-	id: string;
+  data: { [key: string]: any };
+  id: string;
 }): Promise<DataResponse<Transaction[]>> =>
-	apiRequest({
-		url: `${ENDPOINTS.GiftCard}/${payload.id}`,
-		method: 'PUT',
-		data: payload.data,
-	});
+  apiRequest({
+    url: `${ENDPOINTS.GiftCard}/${payload.id}`,
+    method: 'PUT',
+    data: payload.data,
+  });
+
+export const updateGiftCardTransactionStatus = async (payload: {
+  data: {
+    operation: 'SUCCESSFUL' | 'FAILED';
+  };
+  id: string;
+}): Promise<DataResponse<Transaction[]>> =>
+  apiRequest({
+    url: `${ENDPOINTS.GiftCard}/operations/${payload.id}`,
+    method: 'PUT',
+    data: payload.data,
+  });

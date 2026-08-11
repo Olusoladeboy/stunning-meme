18749c3023aa798f33704fce356ab41400a3524b
Emmanuel Tochukwu <to.emmanuel93@gmail.com>
2026-08-11 19:14:19 +0100

    App update

--- src/pages/conversions/auto-conversions/index.tsx
+++ src/pages/conversions/auto-conversions/index.tsx
@@ -5,212 +5,212 @@ import { Box, Typography, useTheme } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import { useQuery, useQueryClient } from 'react-query';
 import {
-	Layout,
-	AutoConversionsTable,
-	ConversionTotal,
-	AvailableNetwork,
-	Pagination,
-	AutoConversionStatistics,
+  Layout,
+  AutoConversionsTable,
+  ConversionTotal,
+  AvailableNetwork,
+  Pagination,
+  AutoConversionStatistics,
 } from 'components';
 import {
-	BOX_SHADOW,
-	QueryKeys,
-	MAX_RECORDS,
-	LINKS,
-	ErrorBoundary,
+  BOX_SHADOW,
+  QueryKeys,
+  MAX_RECORDS,
+  LINKS,
+  ErrorBoundary,
 } from 'utilities';
 import { useAppSelector } from 'store/hooks';
 import {
-	useAlert,
-	useHandleError,
-	usePageTitle,
-	useSearchConversion,
+  useAlert,
+  useHandleError,
+  usePageTitle,
+  useSearchConversion,
 } from 'hooks';
 import { autoConvertAirtimeGroups } from 'api';
 
 const AutoConversions = () => {
-	usePageTitle('Auto Conversion');
-	const queryClient = useQueryClient();
-	const theme = useTheme();
-	const handleError = useHandleError();
-	const setAlert = useAlert();
-	const styles = useStyles(theme);
-	const [isReload, setReload] = useState<boolean>(false);
-	const [sort, setSort] = useState('-createdAt');
-	const [isReloading, setReloading] = useState<boolean>(false);
-	const navigate = useNavigate();
-	const [count, setCount] = useState<number>(1);
-	const [page, setPage] = useState<number>(1);
-	const [total, setTotal] = useState<number>(0);
+  usePageTitle('Auto Conversion');
+  const queryClient = useQueryClient();
+  const theme = useTheme();
+  const handleError = useHandleError();
+  const setAlert = useAlert();
+  const styles = useStyles(theme);
+  const [isReload, setReload] = useState<boolean>(false);
+  const [sort, setSort] = useState('-createdAt');
+  const [isReloading, setReloading] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const [count, setCount] = useState<number>(1);
+  const [page, setPage] = useState<number>(1);
+  const [total, setTotal] = useState<number>(0);
 
-	const location = useLocation();
-	const query = queryString.parse(location.search);
-	const authState = useAppSelector((store) => store.authState);
+  const location = useLocation();
+  const query = queryString.parse(location.search);
+  const authState = useAppSelector((store) => store.authState);
 
-	const token = authState.token;
-	const canViewStatistics = authState.canViewStatistics;
+  const token = authState.token;
+  const canViewStatistics = authState.canViewStatistics;
 
-	const statistics = useAppSelector((store) => store.appState.statistics);
+  const statistics = useAppSelector((store) => store.appState.statistics);
 
-	const { isSearching, search, clearSearch, searchConversion } =
-		useSearchConversion({
-			isAutoConvert: true,
-		});
+  const { isSearching, search, clearSearch, searchConversion } =
+    useSearchConversion({
+      isAutoConvert: true,
+    });
 
-	useEffect(() => {
-		if (query && query.page) {
-			setPage(parseInt(query.page as string));
-		}
-	}, [query, query.page]);
+  useEffect(() => {
+    if (query && query.page) {
+      setPage(parseInt(query.page as string));
+    }
+  }, [query, query.page]);
 
-	useEffect(() => {
-		setReload(true);
-	}, []);
+  useEffect(() => {
+    setReload(true);
+  }, []);
 
-	const params = {
-		limit: MAX_RECORDS,
-		skip: (page - 1) * MAX_RECORDS,
-		sort,
-		populate: 'network,user',
-	};
+  const params = {
+    limit: MAX_RECORDS,
+    skip: (page - 1) * MAX_RECORDS,
+    sort,
+    populate: 'network,user',
+  };
 
-	const { isLoading, data, refetch } = useQuery(
-		[QueryKeys.AutoConvertAirtime, page],
-		() => autoConvertAirtimeGroups(params),
-		{
-			enabled: !!(token || isReload),
-			keepPreviousData: true,
-			refetchOnWindowFocus: false,
-			onSettled: (data, error) => {
-				setReload(false);
-				setReloading(false);
-				if (error) {
-					const response = handleError({ error });
-					if (response?.message)
-						setAlert({ message: response.message, type: 'error' });
-				}
+  const { isLoading, data, refetch } = useQuery(
+    [QueryKeys.AutoConvertAirtime, page],
+    () => autoConvertAirtimeGroups(params),
+    {
+      enabled: !!(token || isReload),
+      keepPreviousData: true,
+      refetchOnWindowFocus: false,
+      onSettled: (data, error) => {
+        setReload(false);
+        setReloading(false);
+        if (error) {
+          const response = handleError({ error });
+          if (response?.message)
+            setAlert({ message: response.message, type: 'error' });
+        }
 
-				if (data && data.success) {
-					const total = data.metadata.total;
-					setTotal(data.metadata.total);
-					const count = Math.ceil(total / MAX_RECORDS);
-					setCount(count);
-				}
-			},
-		},
-	);
+        if (data && data.success) {
+          const total = data.metadata.total;
+          setTotal(data.metadata.total);
+          const count = Math.ceil(total / MAX_RECORDS);
+          setCount(count);
+        }
+      },
+    },
+  );
 
-	const handlePageChange = (page: number) => {
-		setReload(true);
-		if (page !== 1) {
-			setPage(page);
-			navigate(`${LINKS.AutoConversions}?page=${page}`);
-		} else {
-			navigate(LINKS.AutoConversions);
-			setPage(page);
-		}
-	};
+  const handlePageChange = (page: number) => {
+    setReload(true);
+    if (page !== 1) {
+      setPage(page);
+      navigate(`${LINKS.AutoConversions}?page=${page}`);
+    } else {
+      navigate(LINKS.AutoConversions);
+      setPage(page);
+    }
+  };
 
-	const handleSort = (sort: string) => {
-		if (data) {
-			setSort(sort);
-			refetch();
-		}
-	};
+  const handleSort = (sort: string) => {
+    if (data) {
+      setSort(sort);
+      refetch();
+    }
+  };
 
-	return (
-		<Layout>
-			<Box style={styles.container}>
-				<Box
-					sx={{
-						padding: { xs: '0px 15px', md: '0px 2rem' },
-						marginBottom: '2rem',
-					}}
-				>
-					<Box
-						sx={{
-							marginBottom: theme.spacing(4),
-							display: 'flex',
-							gap: '15px',
-							alignItems: 'center',
-							justifyContent: 'space-between',
-						}}
-					>
-						<Typography sx={{ fontWeight: 'bold' }} variant={'h5'}>
-							Auto Conversions
-						</Typography>
-					</Box>
-					{canViewStatistics && (
-						<Box
-							sx={{
-								display: 'grid',
-								gap: ['15px', '30px'],
-							}}
-						>
-							<Box
-								sx={{
-									display: 'grid',
-									gridTemplateColumns: {
-										xs: '1fr',
-										lg: 'repeat(2, 1fr)',
-									},
-									gap: {
-										xs: theme.spacing(3),
-										lg: theme.spacing(5),
-									},
-								}}
-							>
-								<ConversionTotal
-									handleRefresh={() => {
-										refetch();
-										setReloading(true);
-										queryClient.invalidateQueries([QueryKeys.Statistics]);
-									}}
-									total={data && data.metadata.total}
-									totalAmount={statistics?.total_auto_airtime_converted || 0}
-								/>
-								<AvailableNetwork type={'auto'} />
-							</Box>
+  return (
+    <Layout>
+      <Box style={styles.container}>
+        <Box
+          sx={{
+            padding: { xs: '0px 15px', md: '0px 2rem' },
+            marginBottom: '2rem',
+          }}
+        >
+          <Box
+            sx={{
+              marginBottom: theme.spacing(4),
+              display: 'flex',
+              gap: '15px',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography sx={{ fontWeight: 'bold' }} variant={'h5'}>
+              Auto Conversions
+            </Typography>
+          </Box>
+          {canViewStatistics && (
+            <Box
+              sx={{
+                display: 'grid',
+                gap: ['15px', '30px'],
+              }}
+            >
+              <Box
+                sx={{
+                  display: 'grid',
+                  gridTemplateColumns: {
+                    xs: '1fr',
+                    lg: 'repeat(2, 1fr)',
+                  },
+                  gap: {
+                    xs: theme.spacing(3),
+                    lg: theme.spacing(5),
+                  },
+                }}
+              >
+                <ConversionTotal
+                  handleRefresh={() => {
+                    refetch();
+                    setReloading(true);
+                    queryClient.invalidateQueries([QueryKeys.Statistics]);
+                  }}
+                  total={data && data.metadata.total}
+                  totalAmount={statistics?.total_auto_airtime_converted || 0}
+                />
+                <AvailableNetwork type={'auto'} />
+              </Box>
 
-							<AutoConversionStatistics />
-						</Box>
-					)}
-				</Box>
-				<ErrorBoundary>
-					<AutoConversionsTable
-						isDisplaySearchField
-						isLoading={isLoading || isReloading || isSearching}
-						conversions={search ? search : data && data.payload}
-						handleSort={handleSort}
-						handleSearch={searchConversion}
-						clearSearch={clearSearch}
-					/>
+              <AutoConversionStatistics />
+            </Box>
+          )}
+        </Box>
+        <ErrorBoundary>
+          <AutoConversionsTable
+            isDisplaySearchField
+            isLoading={isLoading || isReloading || isSearching}
+            conversions={search ? search : data && data.payload}
+            handleSort={handleSort}
+            handleSearch={searchConversion}
+            clearSearch={clearSearch}
+          />
 
-					{!search && total > MAX_RECORDS && !isReloading && (
-						<Pagination
-							sx={{ marginTop: '2rem', marginLeft: ['15px', '30px'] }}
-							size={'large'}
-							variant={'outlined'}
-							shape={'rounded'}
-							page={page}
-							count={count}
-							onChange={(e, number) => handlePageChange(number)}
-						/>
-					)}
-				</ErrorBoundary>
-			</Box>
-		</Layout>
-	);
+          {!search && total > MAX_RECORDS && !isReloading && (
+            <Pagination
+              sx={{ marginTop: '2rem', marginLeft: ['15px', '30px'] }}
+              size={'large'}
+              variant={'outlined'}
+              shape={'rounded'}
+              page={page}
+              count={count}
+              onChange={(e, number) => handlePageChange(number)}
+            />
+          )}
+        </ErrorBoundary>
+      </Box>
+    </Layout>
+  );
 };
 
 const useStyles = (theme: any) => ({
-	container: {
-		border: `0.5px solid ${theme.palette.secondary.main}`,
-		padding: '1.5rem 0px',
-		backgroundColor: grey[50],
-		borderRadius: theme.spacing(2),
-		boxShadow: BOX_SHADOW,
-	},
+  container: {
+    border: `0.5px solid ${theme.palette.secondary.main}`,
+    padding: '1.5rem 0px',
+    backgroundColor: grey[50],
+    borderRadius: theme.spacing(2),
+    boxShadow: BOX_SHADOW,
+  },
 });
 
 export default AutoConversions;
--- src/pages/transactions/withdrawal-request.tsx
+++ src/pages/transactions/withdrawal-request.tsx
@@ -1,175 +1,284 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  CSSProperties,
+} from 'react';
 import queryString from 'query-string';
 import { Box, useTheme } from '@mui/material';
 import { useQuery } from 'react-query';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { grey } from '@mui/material/colors';
 import {
-	Layout,
-	TableHeader,
-	TablePagination,
-	WithdrawalTransactionsTable,
+  Button,
+  Layout,
+  ModalWrapper,
+  TableHeader,
+  TablePagination,
+  WithdrawalTransactionsTable,
 } from 'components';
 import {
-	BOX_SHADOW,
-	MAX_RECORDS,
-	LINKS,
-	RouteGuard,
-	ADMIN_ROLE,
+  BOX_SHADOW,
+  MAX_RECORDS,
+  LINKS,
+  RouteGuard,
+  ADMIN_ROLE,
+  SECOUNDARY_COLOR,
+  capitalize,
 } from 'utilities';
 import { walletWithdrawal } from 'api';
 import {
-	useHandleError,
-	useAlert,
-	useSearchTransaction,
-	usePageTitle,
+  useHandleError,
+  useAlert,
+  useSearchTransaction,
+  usePageTitle,
 } from 'hooks';
+import DatePicker from 'components/form-components/date-picker';
+import moment from 'moment';
 
 const WithdrawalRequestTransactions = () => {
-	usePageTitle('Withdrawal Request');
-	const theme = useTheme();
-	const handleError = useHandleError();
-	const styles = useStyles(theme);
-	const alert = useAlert();
-	const navigate = useNavigate();
-	// const [count, setCount] = useState<number>(1);
-	const location = useLocation();
-	const query = queryString.parse(location.search);
-	const [page, setPage] = useState<number>(Number(query?.page) || 1);
-	const [total, setTotal] = useState<number>(0);
-	const maxRecordRef = useRef<number>(MAX_RECORDS);
+  usePageTitle('Withdrawal Request');
+  const theme = useTheme();
+  const handleError = useHandleError();
+  const styles = useStyles(theme);
+  const [isDisplayPicker, setDisplayPicker] = useState<boolean>(false);
+  const alert = useAlert();
+  const navigate = useNavigate();
+  // const [count, setCount] = useState<number>(1);
+  const location = useLocation();
+  const query = queryString.parse(location.search);
+  const [page, setPage] = useState<number>(Number(query?.page) || 1);
+  const [total, setTotal] = useState<number>(0);
+  const maxRecordRef = useRef<number>(MAX_RECORDS);
+  const [dateRange, setDateRange] = useState<{ [key: string]: string }>({});
 
-	const { isSearching, searchTransaction, clearSearch, search } =
-		useSearchTransaction();
+  const startDate = useRef<string>('');
+  const endDate = useRef<string>('');
 
-	useEffect(
-		() => {
-			if (query && query.page) {
-				setPage(parseInt(query.page as string));
-			}
-		},
-		// eslint-disable-next-line
-		[query],
-	);
+  const { isSearching, searchTransaction, clearSearch, search } =
+    useSearchTransaction();
 
-	const { isLoading, data, refetch } = useQuery(
-		['Withdrawal', query.page],
-		() =>
-			walletWithdrawal({
-				sort: '-createdAt',
-				limit: maxRecordRef.current,
-				skip: (page - 1) * maxRecordRef.current,
-				populate: 'user',
-				status: 'PENDING',
-				canRequery: false,
-			}),
-		{
-			retry: 2,
-			refetchOnWindowFocus: false,
-			onSettled: (data: any, error) => {
-				if (error) {
-					const response = handleError({ error });
-					if (response?.message) {
-						alert({ message: response.message, type: 'error' });
-					}
-				}
-				if (data && data.success) {
-					const total = data.metadata.total;
-					setTotal(total);
-					// const count = Math.ceil(total / maxRecordRef.current);
-					// setCount(count);
-				}
-			},
-		},
-	);
+  useEffect(
+    () => {
+      if (query && query.page) {
+        setPage(parseInt(query.page as string));
+      }
+    },
+    // eslint-disable-next-line
+    [query],
+  );
 
-	const handlePageChange = (page: number) => {
-		if (page !== 1) {
-			setPage(page);
-			navigate(`${LINKS.WithdrawalRequestTransactions}?page=${page}`);
-		} else {
-			navigate(LINKS.WithdrawalRequestTransactions);
-			setPage(page);
-		}
-		refetch();
-	};
+  const { isLoading, data, refetch } = useQuery(
+    ['Withdrawal', query.page, dateRange],
+    () =>
+      walletWithdrawal({
+        sort: '-createdAt',
+        limit: maxRecordRef.current,
+        skip: (page - 1) * maxRecordRef.current,
+        populate: 'user',
+        status: 'PENDING',
+        canRequery: false,
+        ...(Object.values(dateRange).length > 0 && dateRange),
+      }),
+    {
+      retry: 2,
+      refetchOnWindowFocus: false,
+      onSettled: (data: any, error) => {
+        if (error) {
+          const response = handleError({ error });
+          if (response?.message) {
+            alert({ message: response.message, type: 'error' });
+          }
+        }
+        if (data && data.success) {
+          const total = data.metadata.total;
+          setTotal(total);
+          // const count = Math.ceil(total / maxRecordRef.current);
+          // setCount(count);
+        }
+      },
+    },
+  );
 
-	const handleChangeRowsPerPage = (value: number) => {
-		maxRecordRef.current = value;
-		refetch();
-	};
+  const handlePageChange = (page: number) => {
+    if (page !== 1) {
+      setPage(page);
+      navigate(`${LINKS.WithdrawalRequestTransactions}?page=${page}`);
+    } else {
+      navigate(LINKS.WithdrawalRequestTransactions);
+      setPage(page);
+    }
+    refetch();
+  };
 
-	return (
-		<Layout>
-			<RouteGuard roles={[ADMIN_ROLE.SUPER_ADMIN]}>
-				<Box style={styles.container}>
-					<Box
-						sx={{
-							padding: { xs: '0px 15px', md: '0px 2rem' },
-							display: 'grid',
-							gap: '2rem',
-						}}
-					>
-						<TableHeader
-							searchPlaceholder={'Search transaction by reference'}
-							title={'Withdrawal  Request'}
-							handleSearch={(value) => searchTransaction({ value })}
-							clearSearch={clearSearch}
-						/>
-					</Box>
+  const handleChangeRowsPerPage = (value: number) => {
+    maxRecordRef.current = value;
+    refetch();
+  };
 
-					<WithdrawalTransactionsTable
-						isWithdrawalRequest
-						hasActionButton
-						isLoading={isLoading || isSearching}
-						data={search && search.length > 0 ? search : data && data.payload}
-					/>
+  const handleSetDateRange = (dateRange: any) => {
+    // Clear state
+    startDate.current = '';
+    endDate.current = '';
 
-					{!Boolean(search && search.length > 0) &&
-						!isSearching &&
-						!isLoading &&
-						total > maxRecordRef.current && (
-							<Box style={styles.paginationWrapper}>
-								<TablePagination
-									page={page - 1}
-									count={Number(total)}
-									onPageChange={(value) => handlePageChange(value + 1)}
-									rowsPerPage={maxRecordRef.current}
-									handleChangeRowsPerPage={handleChangeRowsPerPage}
-								/>
-							</Box>
-						)}
-				</Box>
-			</RouteGuard>
-		</Layout>
-	);
+    const rangeStartDate = moment(dateRange.startDate).format('YYYY-MM-DD');
+    const rangeEndDate = moment(dateRange.endDate).format('YYYY-MM-DD');
+
+    if (rangeStartDate === rangeEndDate) {
+      startDate.current = rangeStartDate;
+    } else {
+      startDate.current = rangeStartDate;
+      endDate.current = rangeEndDate;
+    }
+  };
+
+  const onApplyDateFilter = useCallback(
+    () => {
+      let payload: { [key: string]: any } = {};
+
+      let dateRange = '';
+
+      if (startDate.current) {
+        payload.start_date = startDate.current;
+        dateRange += `createdAt>=${startDate.current}`;
+      }
+      if (endDate.current) {
+        payload.end_date = endDate.current;
+        dateRange += `&createdAt<=${endDate.current}`;
+      }
+
+      const searchParams = new URLSearchParams(dateRange);
+      setDateRange(Object.fromEntries(searchParams));
+      setTimeout(() => {
+        refetch();
+      }, 500);
+    },
+    // eslint-disable-next-line
+    [startDate.current, endDate.current],
+  );
+
+  return (
+    <Layout>
+      {isDisplayPicker && (
+        <ModalWrapper
+          title={`Filter  Transaction`}
+          contentWidth='700px'
+          closeModal={() => setDisplayPicker(false)}
+        >
+          <DatePicker
+            cancelPicker={() => setDisplayPicker(false)}
+            setDateRange={handleSetDateRange}
+            customButton={
+              <Button
+                sx={{
+                  backgroundColor: `${SECOUNDARY_COLOR} !important`,
+                  color: 'white',
+                  marginTop: '10px',
+                  minWidth: ['120px'],
+                }}
+                onClick={() => {
+                  setDisplayPicker(false);
+                  onApplyDateFilter();
+                }}
+              >
+                Apply
+              </Button>
+            }
+          />
+        </ModalWrapper>
+      )}
+      <RouteGuard roles={[ADMIN_ROLE.SUPER_ADMIN]}>
+        <Box style={styles.container}>
+          <Box
+            sx={{
+              padding: { xs: '0px 15px', md: '0px 2rem' },
+              display: 'grid',
+              gap: '2rem',
+            }}
+          >
+            <TableHeader
+              searchPlaceholder={'Search transaction by reference'}
+              title={'Withdrawal  Request'}
+              handleSearch={(value) => searchTransaction({ value })}
+              clearSearch={clearSearch}
+              statusFilter={
+                <Box sx={{ display: 'flex', gap: '15px' }}>
+                  <Button
+                    size='large'
+                    style={styles.button as CSSProperties}
+                    onClick={(e) => setDisplayPicker(true)}
+                    variant={'outlined'}
+                  >
+                    Filter by date range
+                  </Button>
+                  {data?.payload?.length > 0 && (
+                    <Button
+                      size='large'
+                      style={styles.button as CSSProperties}
+                      onClick={(e) => setDisplayPicker(true)}
+                      variant={'outlined'}
+                    >
+                      Filter by date range
+                    </Button>
+                  )}
+                </Box>
+              }
+            />
+          </Box>
+
+          <WithdrawalTransactionsTable
+            isWithdrawalRequest
+            hasActionButton
+            isLoading={isLoading || isSearching}
+            data={search && search.length > 0 ? search : data && data.payload}
+          />
+
+          {!Boolean(search && search.length > 0) &&
+            !isSearching &&
+            !isLoading &&
+            total > maxRecordRef.current && (
+              <Box style={styles.paginationWrapper}>
+                <TablePagination
+                  page={page - 1}
+                  count={Number(total)}
+                  onPageChange={(value) => handlePageChange(value + 1)}
+                  rowsPerPage={maxRecordRef.current}
+                  handleChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+              </Box>
+            )}
+        </Box>
+      </RouteGuard>
+    </Layout>
+  );
 };
 
 const useStyles = (theme: any) => ({
-	container: {
-		display: 'grid',
-		gridTemplateColumn: '1fr',
-		gap: theme.spacing(4),
-		border: `0.5px solid ${theme.palette.secondary.main}`,
-		padding: '1.5rem 0px',
-		backgroundColor: grey[50],
-		borderRadius: theme.spacing(2),
-		boxShadow: BOX_SHADOW,
-	},
-	paginationWrapper: {
-		display: 'flex',
-		justifyContent: 'flex-end',
-		paddingRight: '20px',
-	},
-	button: {
-		whiteSpace: 'nowrap',
-	},
-	list: {
-		border: `1px solid ${theme.palette.primary.main}`,
-		borderRadius: theme.spacing(1),
-		backgroundColor: theme.palette.background.paper,
-		marginTop: theme.spacing(2),
-	},
+  container: {
+    display: 'grid',
+    gridTemplateColumn: '1fr',
+    gap: theme.spacing(4),
+    border: `0.5px solid ${theme.palette.secondary.main}`,
+    padding: '1.5rem 0px',
+    backgroundColor: grey[50],
+    borderRadius: theme.spacing(2),
+    boxShadow: BOX_SHADOW,
+  },
+  paginationWrapper: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    paddingRight: '20px',
+  },
+  button: {
+    whiteSpace: 'nowrap',
+  },
+  list: {
+    border: `1px solid ${theme.palette.primary.main}`,
+    borderRadius: theme.spacing(1),
+    backgroundColor: theme.palette.background.paper,
+    marginTop: theme.spacing(2),
+  },
 });
 
 export default WithdrawalRequestTransactions;
